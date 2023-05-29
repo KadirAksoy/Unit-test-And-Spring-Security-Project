9316bf44b9867f2f884016078deef5a4533a5fe2
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2023-05-29 18:41:39 +0300

    initial commit

---
+++ src/main/java/com/kadiraksoy/UnittestAndSpringSecurityProject/service/MisketService.java
@@ -0,0 +1,17 @@
+package com.kadiraksoy.UnittestAndSpringSecurityProject.service;
+
+
+import com.kadiraksoy.UnittestAndSpringSecurityProject.repository.MisketRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MisketService {
+
+    private final MisketRepository misketRepository;
+
+    public MisketService(MisketRepository misketRepository) {
+        this.misketRepository = misketRepository;
+    }
+
+
+}
