7b6ee05f7f2b1015eb24db575fcd78d464d98926
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2023-05-29 18:18:00 +0300

    Initial commit

---
+++ src/main/java/com/kadiraksoy/UnittestAndSpringSecurityProject/UnitTestAndSpringSecurityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kadiraksoy.UnittestAndSpringSecurityProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestAndSpringSecurityProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestAndSpringSecurityProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kadiraksoy/UnittestAndSpringSecurityProject/UnitTestAndSpringSecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kadiraksoy.UnittestAndSpringSecurityProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestAndSpringSecurityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
