5f4d2e4cdaf592009ae7920e8ca1c3092aa246b0
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2023-05-30 13:59:21 +0300

    AuthController added

---
+++ src/main/java/com/kadiraksoy/UnittestAndSpringSecurityProject/controller/AuthController.java
@@ -0,0 +1,78 @@
+package com.kadiraksoy.UnittestAndSpringSecurityProject.controller;
+
+
+import com.kadiraksoy.UnittestAndSpringSecurityProject.dto.AuthResponseDTO;
+import com.kadiraksoy.UnittestAndSpringSecurityProject.dto.LoginDto;
+import com.kadiraksoy.UnittestAndSpringSecurityProject.dto.RegisterDto;
+import com.kadiraksoy.UnittestAndSpringSecurityProject.model.Role;
+import com.kadiraksoy.UnittestAndSpringSecurityProject.model.User;
+import com.kadiraksoy.UnittestAndSpringSecurityProject.repository.RoleRepository;
+import com.kadiraksoy.UnittestAndSpringSecurityProject.repository.UserRepository;
+import com.kadiraksoy.UnittestAndSpringSecurityProject.security.JwtGenerator;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+
+@RestController
+@RequestMapping("api/auth")
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtGenerator jwtGenerator;
+
+
+    public AuthController(AuthenticationManager authenticationManager,
+                          UserRepository userRepository,
+                          RoleRepository roleRepository,
+                          PasswordEncoder passwordEncoder,
+                          JwtGenerator jwtGenerator) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtGenerator = jwtGenerator;
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginDto loginDto){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDto.getUsername(),
+                        loginDto.getPassword()
+                ));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = jwtGenerator.generateToke(authentication);
+        return new ResponseEntity<>(new AuthResponseDTO(token), HttpStatus.OK);
+    }
+
+    @PostMapping("register")
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
+        if (userRepository.existsByUsername(registerDto.getUsername())) {
+            return new ResponseEntity<>("Username is taken!", HttpStatus.BAD_REQUEST);
+        }
+
+        User user = new User();
+        user.setUsername(registerDto.getUsername());
+        user.setPassword(passwordEncoder.encode((registerDto.getPassword())));
+
+        Role roles = roleRepository.findByName("USER").get();
+        user.setRoles(Collections.singletonList(roles));
+
+        userRepository.save(user);
+
+        return new ResponseEntity<>("User registered success!", HttpStatus.OK);
+    }
+}
