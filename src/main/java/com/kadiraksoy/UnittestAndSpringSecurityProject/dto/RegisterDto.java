d8b421a1bf44b25e0a576191be11663e3c3a7bf1
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2023-05-30 13:50:40 +0300

    Login,register and authresponse dto class added

---
+++ src/main/java/com/kadiraksoy/UnittestAndSpringSecurityProject/dto/RegisterDto.java
@@ -0,0 +1,11 @@
+package com.kadiraksoy.UnittestAndSpringSecurityProject.dto;
+
+
+import lombok.Data;
+
+@Data
+public class RegisterDto {
+
+    private String username;
+    private String password;
+}
