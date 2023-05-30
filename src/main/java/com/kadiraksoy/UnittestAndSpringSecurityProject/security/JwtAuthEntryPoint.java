97e30c146f5c18080b9651665a2197424607dc08
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2023-05-30 13:19:57 +0300

    JwtAuthEntryPoint added

---
+++ src/main/java/com/kadiraksoy/UnittestAndSpringSecurityProject/security/JwtAuthEntryPoint.java
@@ -0,0 +1,26 @@
+package com.kadiraksoy.UnittestAndSpringSecurityProject.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
+
+
+    // This method is executed when authentication fails and
+    // access to a protected resource requiring authorization is requested.
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+
+    }
+}
